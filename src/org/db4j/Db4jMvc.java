1db0bb7973caf0af990188ee37bcf223b21234a4
lytles@takashi <srl.source@nqzero.com>
2018-04-17 17:29:58 -0400

    db4j.mvc - easier config of gson and exception handling

--- src/org/db4j/Db4jMvc.java
+++ src/org/db4j/Db4jMvc.java
@@ -11,22 +11,28 @@ import kilim.http.KilimMvc;
 
 public class Db4jMvc extends KilimMvc {
     boolean yoda = true;
-    static Gson gson = new Gson();
+    static Gson gsonStatic = new Gson();
+    public Gson gson = gsonStatic;
     static JsonParser parser = new JsonParser();
 
     public <PP extends Db4jRouter> Db4jMvc(Scannable<PP> source,Preppable<PP> auth) {
         scan(source,auth);
     }
 
-    public Db4jMvc start(int port) throws IOException {
-        new kilim.http.HttpServer(port,() -> new Session(this::handle));
-        return this;
+    public kilim.http.HttpServer start(int port) throws IOException {
+        return new kilim.http.HttpServer(port,() -> new Session(this::handle));
+    }
+    protected <PP extends Router> PP supply(Scannable<PP> source,Consumer<Route> router) {
+        // fixme - make KilimMvc generic
+        PP pp = super.supply(source,router);
+        ((Db4jRouter) pp).gson = gson;
+        return pp;
     }
 
     public static class Db4jRouter<PP extends Db4jRouter> extends Router<PP> {
         public Db4j db4j;
-        public Gson gson = Db4jMvc.gson;
-        boolean logExtra = false;
+        public Gson gson;
+        protected boolean logExtra = false;
 
         protected Db4jRouter(Consumer<Route> mk) { super(mk); }
 
@@ -41,16 +47,16 @@ public class Db4jMvc extends KilimMvc {
             db4j.submitCall(body).await();
         }
 
-        public Gson gson() { return Db4jMvc.gson; }
+        public Gson gson() { return gson; }
         public <TT> TT fromJson(String txt,Class<TT> klass) { return gson().fromJson(txt,klass); }
         public String toJson(Object obj) { return gson().toJson(obj); }
-        public Object parse(String txt) { return parser.parse(txt); }
+        public Object parseJson(String txt) { return parser.parse(txt); }
         public void logExtra(String txt,Object val) {
             if (! logExtra)
                 return;
-            Object parsed = parse(txt);
-            String v1 = toJson(val);
-            String v2 = toJson(parsed);
+            Object parsed = parseJson(txt);
+            String v1 = gsonStatic.toJson(val);
+            String v2 = gsonStatic.toJson(parsed);
             if (! v1.equals(v2)) {
                 System.out.format("%-40s --> %s\n",req.uriPath,val.getClass().getName());
                 System.out.println("\t" + v1);
@@ -65,10 +71,10 @@ public class Db4jMvc extends KilimMvc {
             logExtra(txt,val);
             return val;
         }
-        String body() {
+        public String body() {
             return req.extractRange(req.contentOffset,req.contentOffset+req.contentLength);
         }
-        byte [] rawBody() {
+        public byte [] rawBody() {
             return req.extractBytes(req.contentOffset,req.contentOffset+req.contentLength);
         }
     }
@@ -95,19 +101,21 @@ public class Db4jMvc extends KilimMvc {
     public static HttpStatus badRequest(String message) {
         return new HttpStatus(message,HttpResponse.ST_BAD_REQUEST);
     }
+    public Object handleEx(Session session,HttpRequest req,HttpResponse resp,Exception ex) {
+        if (ex instanceof HttpStatus)
+            return ((HttpStatus) ex).route(resp);
+        resp.status = HttpResponse.ST_BAD_REQUEST;
+        return ex.getMessage();
+    }
     public void handle(Session session,HttpRequest req,HttpResponse resp) throws Pausable, Exception {
         Object reply;
 
         if (yoda)
             try { reply = route(session,req,resp); }
-            catch (HttpStatus ex) { reply = ex.route(resp); }
+            catch (HttpStatus ex) { reply = handleEx(session,req,resp,ex); }
         else
             try { reply = route(session,req,resp); }
-            catch (HttpStatus ex) { reply = ex.route(resp); }
-            catch (Exception ex) {
-                resp.status = HttpResponse.ST_BAD_REQUEST;
-                reply = ex.getMessage();
-            }
+            catch (Exception ex) { reply = handleEx(session,req,resp,ex); }
 
         if (reply != null) {
             write(resp,reply);
